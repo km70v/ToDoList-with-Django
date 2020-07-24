46751ae03e2bbe02cfc34d3e4f96f6a37225957f
kcy <kim70v@naver.com>
2020-07-24 14:26:23 +0900

    version 1

---
+++ ToDoList/my_to_do_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
+
+# Create your views here.
+def index(request):
+    todos = Todo.objects.all()
+    content = {'todos':todos}
+    return render(request, 'my_to_do_app/index.html', content)
+
+def createTodo(request):
+    usr_input_str = request.POST['todoContent']
+    new_todo = Todo(content = usr_input_str)
+    new_todo.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def doneTodo(request):
+    done_todo_id = request.GET['todoNum']
+    todo = Todo.objects.get(id = done_todo_id)
+    todo.delete()
+    return HttpResponseRedirect(reverse('index'))
